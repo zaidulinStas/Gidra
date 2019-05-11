90e291717e77c14bb3aa0f1b26870eda883f7812
ZaidulinKirill <zaidulinkirill@gmail.com>
2019-05-11 10:19:12 +0300

    Add Process

---
+++ Gidra-dev-interface/Gidra-dev-interface/GidraSIM/GidraSIM.Core.Model/Process.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GidraSIM.Core.Model
+{
+    /// <summary>
+    /// Класс процесса
+    /// </summary>
+    public class Process : BaseProcedure
+    {
+        /// <summary>
+        /// Список процедур
+        /// </summary>
+        public IList<IProcedure> Procedures { get; } = new List<IProcedure>();
+
+        /// <summary>
+        /// Начальная процедура
+        /// </summary>
+        public IProcedure StartProcedure { get; set; }
+
+        /// <summary>
+        /// Конечная процедура
+        /// </summary>
+        public IProcedure EndProcedure { get; set; }
+
+
+        /// <summary>
+        /// Функция, вызывающаяся при начале моделирования 
+        /// </summary>
+        protected override bool OnStartModeling()
+        {
+            var newToken = new Token();
+
+            foreach (var input in StartProcedure.Inputs)
+            {
+                input.Tokens.Enqueue(newToken);
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Обновление моделирования процедуры
+        /// </summary>
+        protected override bool OnUpdateModeling(double curTime)
+        {
+            foreach (var procedure in Procedures)
+            {
+                procedure.Update(curTime);
+            }
+
+            return !EndProcedure.Outputs.Any(x => x.Tokens.Any());
+        }
+
+        /// <summary>
+        /// Функция, вызывающаяся при окончании моделирования 
+        /// </summary>
+        protected override bool OnEndModeling()
+        {
+            foreach (var output in EndProcedure.Outputs)
+            {
+                output.Tokens.Peek();
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Gidra-dev-interface/Gidra-dev-interface/GidraSIM/GidraSIM.Core.Test/ProcedureTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GidraSIM.Core.Model.Procedures;
+
+namespace GidraSIM.Core.Model.Tests
+{
+    [TestClass]
+    public class ProcedureTests
+    {
+        [TestMethod]
+        public void ProcedureWithResources()
+        {
+
+            //#region Тест одновходного блока и
+            //AndBlock andBlock = new AndBlock(1);
+
+            
+            //andBlock.AddToken(new Token(0, 0), 0);
+            ////првоеряем, родил ли блок токен
+            //Assert.IsNotNull(andBlock.GetOutputToken(0));
+            //#endregion
+
+            //#region Тест двувходного блока 
+            //andBlock = new AndBlock(2);
+            //andBlock.AddToken(new Token(1, 2), 0);
+            ////не должен был родить
+            //Assert.IsNull(andBlock.GetOutputToken(0));
+
+            
+            //andBlock.AddToken(new Token(2, 5), 1);
+            //var token = andBlock.GetOutputToken(0);
+            ////должен был родить
+            //Assert.IsNotNull(token);
+
+            ////времмя рождения должно быть как последнее из всех входных, т.е. 2
+            //Assert.AreEqual(2, token.BornTime, 0.1);
+            ////сложность должна быть средним арифмектическим
+            //Assert.AreEqual((2+5)/2.0, token.Complexity, 0.1);
+
+            //TokensCollector tokensCollector = TokensCollector.GetInstance();
+            //var collectorToken1 = tokensCollector.GetHistory()[1];
+            //var collectorToken2 = tokensCollector.GetHistory()[2];
+            ////в коллектор должны были прийти входные
+            //Assert.IsNotNull(collectorToken1);
+            //Assert.IsNotNull(collectorToken2);
+
+            ////время обработки должно быть как последнее время рождения
+            //Assert.AreEqual(token.BornTime, collectorToken1.ProcessEndTime, 0.1);
+            //Assert.AreEqual(token.BornTime, collectorToken2.ProcessEndTime, 0.1);
+            //#endregion
+        }
+    }
+}
